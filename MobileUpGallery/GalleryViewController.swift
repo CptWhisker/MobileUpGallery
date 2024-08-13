761be6f73c0afb3c08ed7bba83820670815dc38e
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-08-14 01:07:03 +0300

    Implement basic UI configuration for `GalleryViewController` with stubs instead of data

--- MobileUpGallery/GalleryViewController.swift
+++ MobileUpGallery/GalleryViewController.swift
@@ -1,7 +1,94 @@
 import UIKit
 
 final class GalleryViewController: UIViewController {
+    private lazy var segmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: ["Фото", "Видео"])
+        control.selectedSegmentIndex = 0
+        control.translatesAutoresizingMaskIntoConstraints = false
+        control.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
+        return control
+    }()
+    private lazy var photosCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .blue
+        return collectionView
+    }()
+    private lazy var videosCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .yellow
+        return collectionView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureInterface()
+    }
+    
+    private func configureInterface() {
+        view.backgroundColor = .white
+        
+        configureNavigationBar()
+        configureSegmentedControl()
+        configureCollectionViews()
+    }
+    
+    private func configureNavigationBar(){
+        title = "MobileUp Gallery"
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Выход", style: .plain, target: self, action: #selector(logoutTapped))
+    }
+    
+    private func configureSegmentedControl() {
+        view.addSubview(segmentedControl)
+        
+        NSLayoutConstraint.activate([
+            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func configureCollectionViews() {
+        view.addSubview(photosCollectionView)
+        view.addSubview(videosCollectionView)
+        
+        NSLayoutConstraint.activate([
+            photosCollectionView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8),
+            photosCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            photosCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            photosCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            videosCollectionView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8),
+            videosCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            videosCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            videosCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        videosCollectionView.isHidden = true
+    }
+    
+    @objc private func segmentChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            photosCollectionView.isHidden = false
+            videosCollectionView.isHidden = true
+        case 1:
+            photosCollectionView.isHidden = true
+            videosCollectionView.isHidden = false
+        default:
+            break
+        }
+    }
+    
+    @objc private func logoutTapped() {
+        if view.backgroundColor != .red {
+            view.backgroundColor = .red
+        } else {
+            view.backgroundColor = .white
+        }
     }
 }
