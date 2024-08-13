45601357e0df330c99604fcc3edabb79b55a6bd9
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-08-13 19:41:08 +0300

    Implement KVO to track the loading process

--- MobileUpGallery/WebViewViewController.swift
+++ MobileUpGallery/WebViewViewController.swift
@@ -8,18 +8,27 @@ final class WebViewViewController: UIViewController {
         webView.translatesAutoresizingMaskIntoConstraints = false
         return webView
     }()
+    private lazy var loadingBar: UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .bar)
+        progressView.progressTintColor = .black
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        return progressView
+    }()
+    private var estimatedProgressObservation: NSKeyValueObservation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureInterface()
+        addObserverForLoadingBar()
+        loadPage()
     }
     
     private func configureInterface() {
         view.backgroundColor = .white
         
         configureWebView()
-        loadPage()
+        configureLoadingBar()
     }
     
     private func configureWebView() {
@@ -33,12 +42,40 @@ final class WebViewViewController: UIViewController {
         ])
     }
     
+    private func configureLoadingBar() {
+        view.addSubview(loadingBar)
+        
+        NSLayoutConstraint.activate([
+            loadingBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            loadingBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            loadingBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
+    private func addObserverForLoadingBar() {
+        estimatedProgressObservation = webView.observe(\.estimatedProgress, options: []) { [weak self] _, _ in
+            guard let self else { return }
+            
+            updateLoadingProgress(webView.estimatedProgress)
+        }
+    }
+    
     private func loadPage() {
         let url = URL(string: "https://google.com")!
         
         let request = URLRequest(url: url)
         
         webView.load(request)
+    }
+    
+    private func updateLoadingProgress(_ newValue: Double) {
+        let newProgressValue = Float(newValue)
+        
+        loadingBar.progress = newProgressValue
+        
+        if abs(newProgressValue - 1.0) <= 0.001 {
+            loadingBar.isHidden = true
+        }
     }
 }
 
