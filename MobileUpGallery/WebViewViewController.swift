9b4050d26d0d045de89f1a42d37e79cb6a768905
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-08-13 22:20:35 +0300

    Add methods to WebViewViewController for loading the authentication web view, handling navigation actions, and extracting the access token; Add annotations for better readability

--- MobileUpGallery/WebViewViewController.swift
+++ MobileUpGallery/WebViewViewController.swift
@@ -22,54 +22,36 @@ final class WebViewViewController: UIViewController {
         super.viewDidLoad()
         
         configureInterface()
-        addObserverForLoadingBar()
-        loadPage()
+        addObserverForLoadingProgress()
+        loadAuthView()
     }
     
     // MARK: - Interface Configuration
     private func configureInterface() {
         view.backgroundColor = .white
         
-        configureWebView()
-        configureLoadingBar()
-    }
-    
-    private func configureWebView() {
         view.addSubview(webView)
+        view.addSubview(loadingBar)
         
         NSLayoutConstraint.activate([
             webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-    }
-    
-    private func configureLoadingBar() {
-        view.addSubview(loadingBar)
-        
-        NSLayoutConstraint.activate([
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
             loadingBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             loadingBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             loadingBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
     }
     
-    // MARK: - Private Methods
-    private func addObserverForLoadingBar() {
+    // MARK: - KVO
+    private func addObserverForLoadingProgress() {
         estimatedProgressObservation = webView.observe(\.estimatedProgress, options: []) { [weak self] _, _ in
             guard let self else { return }
             
-            updateLoadingProgress(webView.estimatedProgress)
+            self.updateLoadingProgress(self.webView.estimatedProgress)
         }
-    }
-    
-    private func loadPage() {
-        let url = URL(string: "https://google.com")!
-        
-        let request = URLRequest(url: url)
-        
-        webView.load(request)
     }
     
     private func updateLoadingProgress(_ newValue: Double) {
@@ -81,9 +63,57 @@ final class WebViewViewController: UIViewController {
             loadingBar.isHidden = true
         }
     }
+    
+    // MARK: - Loading Auth View
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: "https://oauth.vk.com/authorize") else {
+            print("Error: Unable to create URLComponents from unsplashAuthorizeURLString")
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "52136813"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "response_type", value: "token")
+        ]
+        
+        guard let url = urlComponents.url else {
+            print("Error: Unable to create URL from urlComponents")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
+    // MARK: - Extracting Access Token
+    private func getAccessToken(from navigationAction: WKNavigationAction) -> String? {
+        guard let url = navigationAction.request.url,
+              url.path == "/blank.html",
+              let fragment = url.fragment else {
+            return nil
+        }
+        
+        let parameters = Dictionary(uniqueKeysWithValues: fragment.split(separator: "&").map {
+            let pair = $0.split(separator: "=")
+            return (String(pair[0]), String(pair[1]))
+        })
+        
+        return parameters["access_token"]
+    }
+    
+
 }
 
 // MARK: - WKNavigationDelegate
 extension WebViewViewController: WKNavigationDelegate {
-    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let accessToken = getAccessToken(from: navigationAction) {
+            print(accessToken)
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
+    }
 }
