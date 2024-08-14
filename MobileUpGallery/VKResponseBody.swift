a129976f1efcd35398573960c84c7a92ed56dc49
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-08-14 23:30:41 +0300

    Create `VKResponseBody.swift` and Add structs to handle photo decoding

---
+++ MobileUpGallery/VKResponseBody.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+// MARK: - Photos Response
+struct VKPhotosResponse: Decodable {
+    let response: VKPhotoResponse
+}
+
+struct VKPhotoResponse: Decodable {
+    let items: [Photo]
+}
+
+struct Photo: Decodable {
+    let date: Int
+    let sizes: [VKPhotoSize]
+    
+    var thumbURL: String? {
+        guard let thumb = sizes.first(where: { $0.type == "q" }) else {
+            return sizes.first?.url
+        }
+        return thumb.url
+    }
+    
+    var largeURL: String? {
+        return sizes.last?.url
+    }
+}
+
+
+struct VKPhotoSize: Decodable {
+    let type: String
+    let url: String
+}
