8660239c69d1772e75db58eada8d5240f37e8397
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-08-15 00:30:21 +0300

    Create a custom `PhotoCell` utilising Kingfisher to load and display photos

---
+++ MobileUpGallery/PhotoCell.swift
@@ -0,0 +1,41 @@
+import UIKit
+import Kingfisher
+
+final class PhotoCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureCell() {
+        contentView.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    func setPhoto(_ photo: Photo) {
+        guard let urlString = photo.thumbURL,
+              let url = URL(string: urlString)
+        else { return }
+        
+        imageView.kf.setImage(with: url)
+    }
+}
